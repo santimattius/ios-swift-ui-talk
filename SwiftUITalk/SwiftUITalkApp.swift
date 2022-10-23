adb403cf893afa0e928cbc16b3c7efd27d787ea2
Santiago Mattiauda <santimattius@gmail.com>
2022-10-23 19:03:44 -0300

    Initial Commit

---
+++ SwiftUITalk/SwiftUITalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITalkApp.swift
+//  SwiftUITalk
+//
+//  Created by Santiago Mattiauda on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
